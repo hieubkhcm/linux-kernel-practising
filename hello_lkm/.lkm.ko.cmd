69b564dbec778790fcf719b4230412393a593670
root <root@localhost.localdomain>
2021-03-17 10:31:37 +0800

    The first commit

---
+++ hello_lkm/.lkm.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/hello_driver/lkm.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/hello_driver/lkm.ko /home/hello_driver/lkm.o /home/hello_driver/lkm.mod.o;  true
---
+++ hello_lkm/.lkm.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/hello_driver/lkm.mod := { echo  /home/hello_driver/lkm.o; echo; } > /home/hello_driver/lkm.mod
