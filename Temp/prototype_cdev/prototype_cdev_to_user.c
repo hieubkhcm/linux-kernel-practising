6b7654c43abbc254f4a03e30c2b58e9439e5f0bd
root <root@localhost.localdomain>
2021-03-22 11:07:25 +0800

    sync from fedora30 - debugged

--- Temp/prototype_cdev/prototype_cdev_to_user.c
+++ Temp/prototype_cdev/prototype_cdev_to_user.c
@@ -25,17 +25,17 @@ static char c;
 //khai báo hàm() và code hàm luôn
 static int my_open(struct inode *i, struct file *f) // my_open -> Ghi gì cũng được -> match là được
 {
-    printk(KERN_INFO "Driver: open() logging ra để debug\n\n");
+    printk(KERN_INFO "Henry_Driver: open() logging to debug\n");
     return 0;
 }
 static int my_close(struct inode *i, struct file *f) // my_close -> Ghi gì cũng được -> match là được
 {
-    printk(KERN_INFO "Driver: close() logging ra để debug\n\n");
+    printk(KERN_INFO "Henry_Driver: close() logging to debug\n\n");
     return 0;
 }
 static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off)
 {
-    printk(KERN_INFO "Driver: read()\n");
+    printk(KERN_INFO "Henry_Driver: read() logging to debug\n");
     if (*off == 0)
     {
         if (copy_to_user(buf, &c, 1) != 0)
@@ -52,8 +52,8 @@ static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off
 // my_write -> Ghi gì cũng được -> match là được
 static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
 {
-    printk(KERN_INFO "Driver: write()\n");
-    if (copy_from_user(&c, buf + len – 1, 1) != 0)
+    printk(KERN_INFO "Henry_Driver: write() logging to debug\n");
+    if (copy_from_user(&c, buf + len - 1, 1) != 0)
         return -EFAULT;
     else
         return len;
@@ -75,17 +75,17 @@ static int __init ofcd_init(void) /* Constructor */ //__init là để gắn mod
     int ret;
     struct device *dev_ret;
 
-    printk(KERN_INFO "Namaskar: ofcd registered");
-    if ((ret = alloc_chrdev_region(&first, 0, 1, "Shweta")) < 0)
+    printk(KERN_INFO "Henry_log: device registered\n");
+    if ((ret = alloc_chrdev_region(&first, 0, 1, "henry_ko")) < 0)
     {
         return ret;
     }// xong bước này là lấy được cặp số major minor nằm trong dev_t first
-    if (IS_ERR(cl = class_create(THIS_MODULE, "chardrv"))) // chardrv có thể đổi thoải mái, chạy thành công thì trong sys/class sẽ có file chardrv
+    if (IS_ERR(cl = class_create(THIS_MODULE, "henry_class"))) // chardrv có thể đổi thoải mái, chạy thành công thì trong sys/class sẽ có file chardrv
     {
         unregister_chrdev_region(first, 1);
         return PTR_ERR(cl);
     }// xong bước này thì struct class mới gán trị xong để parse tiêp vào hàm dưới device_create
-    if (IS_ERR(dev_ret = device_create(cl, NULL, first, NULL, "mynull")))
+    if (IS_ERR(dev_ret = device_create(cl, NULL, first, NULL, "henry_dev")))
     {
         class_destroy(cl);
         unregister_chrdev_region(first, 1);
@@ -113,10 +113,10 @@ static void __exit ofcd_exit(void) /* Destructor */
     device_destroy(cl, first);
     class_destroy(cl);
     unregister_chrdev_region(first, 1);
-    printk(KERN_INFO "Alvida: ofcd unregistered");
+    printk(KERN_INFO "Henry_log: device unregistered\n");
 }
 
-module_init(ofcd_init);//api để gắn .ko vô kernel
-module_exit(ofcd_exit);//api để gỡ .ko khỏi kernel
+module_init(ofcd_init);//api to attach the .ko to kernel - henry's commnent
+module_exit(ofcd_exit);//api to detach the .ko from kernel - henry's comment
 
 
